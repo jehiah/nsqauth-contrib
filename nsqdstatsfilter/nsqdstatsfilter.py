967706b299d15ae438e90f3e5c20c9db54a9b051
Jehiah Czebotar <jehiah@gmail.com>
2014-06-12 14:10:10 -0400

    filter stats based on lookup for a topic

--- nsqdstatsfilter/nsqdstatsfilter.py
+++ nsqdstatsfilter/nsqdstatsfilter.py
@@ -5,6 +5,7 @@ import tornado.ioloop
 import tornado.options
 import logging
 import urllib
+import functools
 import re
 try:
     import json
@@ -15,21 +16,36 @@ class PingHandler(tornado.web.RequestHandler):
     def get(self):
         self.finish('OK')
 
+
 class StatsProxy(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
+        self.stats = []
         self.auth_servers = list(self.settings["auth_addresses"])
         
         self.http_client = tornado.httpclient.AsyncHTTPClient()
 
-        self.pending = 1
+        self.pending = 0
         self.start_auth()
-        self.http_client.fetch(self.settings["nsqd_http_endpoint"], headers={"Accept": "vnd/nsq; version=1.0"}, callback=self.finish_stats_get)
+        assert len(self.settings["nsqd_endpoints"]) == 1
+        for addr in self.settings["nsqd_endpoints"]:
+            self.start_stats(addr)
+        
+        assert self.pending > 0
+    
+    def start_stats(self, addr):
+        self.pending += 1
+        endpoint = "http://%s/stats?format=json" % addr
+        callback=functools.partial(self.finish_stats_get, addr=addr)
+        logging.info("GET %s", endpoint)
+        self.http_client.fetch(endpoint, headers={"Accept": "vnd/nsq; version=1.0"}, callback=callback)
     
     def start_auth(self):
         secret = self.get_argument("secret")
         self.pending += 1
-        auth_endpoint = "http://%s/auth?%s" % (self.auth_servers.pop(), urllib.urlencode(dict(
+        auth_endpoint = self.auth_servers.pop()
+        logging.info("GET http://%s/auth", auth_endpoint)
+        auth_endpoint = "http://%s/auth?%s" % (auth_endpoint, urllib.urlencode(dict(
             remote_ip=self.request.remote_ip,
             tls='true' if self.request.protocol == 'https' else 'false',
             secret=secret
@@ -39,6 +55,7 @@ class StatsProxy(tornado.web.RequestHandler):
     def finish_auth(self, response):
         self.pending -= 1
         try:
+            logging.debug("response %d %r", response.code, response.body)
             assert response.code == 200
             raw_data = json.loads(response.body)
             self.permissions = raw_data['authorizations']
@@ -52,14 +69,20 @@ class StatsProxy(tornado.web.RequestHandler):
         if self.pending == 0:
             self.filter_stats()
     
-    def finish_stats_get(self, response):
+    def finish_stats_get(self, response, addr):
+        logging.debug("response %d %r", response.code, response.body)
         self.pending -= 1
         assert response.code == 200
         raw_data = json.loads(response.body)
         if "data" in raw_data:
-            self.stats = raw_data["data"]["topics"]
+            topics = raw_data["data"]["topics"]
         else:
-            self.stats = raw_data["topics"]
+            topics = raw_data["topics"]
+
+        for topic in topics:
+            topic["producer"] = addr
+            self.stats.append(topic)
+        
         if self.pending == 0:
             self.filter_stats()
     
@@ -82,8 +105,7 @@ class StatsProxy(tornado.web.RequestHandler):
         return False
     
     def filter_stats(self):
-        
-        output_topics = []
+        output_topics = dict()
         for topic in self.stats:
             output_topic = None
             if self.is_authorized(topic['topic_name'], None, 'publish'):
@@ -93,25 +115,79 @@ class StatsProxy(tornado.web.RequestHandler):
                     backend_depth=topic['backend_depth'],
                     depth=topic['depth'],
                     topic_name=topic['topic_name'],
+                    producers=[topic['producer']],
                     channels=[],
                 )
             elif self.is_authorized(topic['topic_name'], None, 'subscribe'):
                 output_topic = dict(
                     topic_name=topic['topic_name'],
+                    producers=[topic['producer']],
                     channels=[],
                 )
             if not output_topic:
                 continue
-            output_topics.append(output_topic)
+            if topic['topic_name'] in output_topics:
+                existing_topic = output_topics[topic['topic_name']]
+                if 'message_count' in output_topic:
+                    for key in ['message_count', 'backend_depth', 'depth']:
+                        existing_topic[key] += output_topic[key]
+                existing_topic['producers'].extend(output_topic['producers'])
+                output_topic = existing_topic
+            else:
+                output_topics[topic['topic_name']] = output_topic
             
             for channel in topic['channels']:
+                channel['producer'] = topic['producer']
                 if self.is_authorized(topic['topic_name'], None, 'publish') or self.is_authorized(topic['topic_name'], channel['channel_name'], 'subscribe'):
                     output_topic['channels'].append(channel)
         
-        if self.request.headers['Accept'] == "vnd/nsq; version=1.0":
-            self.finish(dict(topics=output_topics))
+        if self.pending == 0:
+            if self.request.headers['Accept'] == "vnd/nsq; version=1.0":
+                self.finish(dict(topics=output_topics.values()))
+            else:
+                self.finish(dict(status_code=200, status_txt="OK", data=dict(topics=output_topics.values())))
+
+
+class TopicStatsProxy(StatsProxy):
+    @tornado.web.asynchronous
+    def get(self):
+        self.stats = []
+        self.auth_servers = list(self.settings["auth_addresses"])
+        self.http_client = tornado.httpclient.AsyncHTTPClient()
+        self.seen_producers = set()
+        topic = self.get_argument("topic")
+
+        self.pending = 0
+        self.start_auth()
+        assert self.settings["lookupd_endpoints"]
+        for endpoint in self.settings["lookupd_endpoints"]:
+            self.pending += 1
+            url = endpoint + '?' + urllib.urlencode(dict(topic=topic, format="json"))
+            logging.info("GET %s", url)
+            self.http_client.fetch(url, headers={"Accept": "vnd/nsq; version=1.0"}, callback=self.finish_lookupd_get)
+        assert self.pending > 0
+    
+    def finish_lookupd_get(self, response):
+        logging.debug("response %d %r", response.code, response.body)
+        # build up the list of producers we need to query
+        self.pending -= 1
+        
+        assert response.code == 200
+        raw_data = json.loads(response.body)
+        if "data" in raw_data:
+            producers = raw_data["data"]["producers"]
         else:
-            self.finish(dict(status_code=200, status_txt="OK", data=dict(topics=output_topics)))
+            producers = raw_data["producers"]
+    
+        for producer in producers:
+            addr = "%s:%d" % (producer["broadcast_address"], producer["http_port"])
+            if addr in self.seen_producers:
+                continue
+            self.seen_producers.add(addr)
+            self.start_stats(addr)
+        
+        if self.pending == 0:
+            self.filter_stats()
 
 
 class Application(tornado.web.Application):
@@ -120,22 +196,30 @@ class Application(tornado.web.Application):
         addresses = tornado.options.options.auth_address
         assert isinstance(addresses, list)
         
+        lookupd_endpoints = []
+        for addr in tornado.options.options.lookupd_http_address:
+            lookupd_endpoints.append("http://%s/lookup" % addr)
+        
         settings = dict(
             debug=tornado.options.options.debug,
             auth_addresses=addresses,
-            nsqd_http_endpoint="http://%s/stats?format=json" % tornado.options.options.nsqd_http_address,
+            nsqd_endpoints=tornado.options.options.nsqd_http_address,
+            lookupd_endpoints=lookupd_endpoints,
         )
         handlers = [
             (r"/ping", PingHandler),
             (r"/stats", StatsProxy),
+            (r"/topic_stats", TopicStatsProxy),
         ]
         super(Application, self).__init__(handlers, **settings)
+
 
 
 if __name__ == "__main__":
     tornado.options.define("http_address", type=str, default="0.0.0.0:4182", help="<addr>:<port> to listen on for HTTP clients")
     tornado.options.define("auth_address", type=str, default="127.0.0.1:4181", multiple=True, help="<addr>:<port> to connect to auth server")
-    tornado.options.define("nsqd_http_address", type=str, default="127.0.0.1:4151", help="<addr>:<port> of nsqd http address for stats call")
+    tornado.options.define("lookupd_http_address", type=str, multiple=True, help="<addr>:<port> to connect to nsqlookupd (specify multiple times)")
+    tornado.options.define("nsqd_http_address", type=str, multiple=True, help="<addr>:<port> of nsqd http address for stats call (skip if using lookupd; specify multiple times)")
     tornado.options.define("debug", type=bool, default=False)
     tornado.options.parse_command_line()
     
